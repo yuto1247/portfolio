a055b629bcc5bf5f1fea626566936656c06d6ca3
yuto1247 <71320971+yuto1247@users.noreply.github.com>
2020-09-17 13:46:50 +0900

    Create viewport.js

---
+++ viewport.js
@@ -0,0 +1,17 @@
+var baseW = 992;
+var ua = navigator.userAgent.toLowerCase();
+var isiPad = (ua.indexOf("ipad") > -1);
+function updateMetaViewport(){
+  var viewportContent;
+  var w = window.outerWidth;
+  if(isiPad){
+    viewportContent = "width="+baseW+"px";
+  } else {
+    viewportContent = "width=device-width,initial-scale=1";
+  }
+  document.querySelector("meta[name='viewport']").setAttribute("content", viewportContent);
+}
+if(isiPad){
+  window.addEventListener("resize", updateMetaViewport, false);
+  window.addEventListener("orientationchange", updateMetaViewport, false);
+}
